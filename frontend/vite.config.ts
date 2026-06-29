e145e23932ea23983a37c9f2e2bff18859a6f4f4
Tirthankar Datta <tirthankardatta904@gmail.com>
2026-06-29 18:16:06 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:8000',
+        target:'https://ai-chat-model-project-1.onrender.com/',
         changeOrigin:true,
         secure:false,
       }
