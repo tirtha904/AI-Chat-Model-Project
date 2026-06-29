f654487ed2156e00b7bbadc638c68e2e8d8809f9
Tirthankar Datta <tirthankardatta904@gmail.com>
2026-06-29 18:21:11 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ const app =express();
 //middlewares
 app.use(express.json())
 app.use(cookieParser(process.env.COOKIE_SECRET))//to invoke cookie from backend to frontend
-app.use(cors({ origin: "http://localhost:5173",credentials:true})) //cors-we can now access the server by this domain 
+app.use(cors({ origin: "https://ai-chat-model-frontend.onrender.com",credentials:true})) //cors-we can now access the server by this domain 
 //remove it in production
 app.use(morgan("dev"))
 app.use("/api/v1",appRouter)
